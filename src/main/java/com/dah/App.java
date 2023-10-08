6d9fbc462d168b79bd8531917eacf3b5a65ad1cb
Xu Zhang <s3800166@student.rmit.edu.au>
2023-10-08 23:33:24 +1100

    working fxml and sample controller

--- src/main/java/com/dah/App.java
+++ src/main/java/com/dah/App.java
@@ -1,13 +1,34 @@
 package com.dah;
 
+import com.dah.controller.LoginController;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends Application  
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("view/LoginView.fxml"));
+        primaryStage.setTitle("FXML Sample");
+
+        // Load the FXML file and set it as the root of the scene
+        primaryStage.setScene(new Scene(loader.load()));
+
+        // Get the controller instance from the loader
+        LoginController controller = loader.getController();
+
+        primaryStage.show();
     }
 }
--- src/main/java/com/dah/controller/LoginController.java
+++ src/main/java/com/dah/controller/LoginController.java
@@ -3,11 +3,20 @@ package com.dah.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
 
 public class LoginController {
     
     @FXML
     private TextField usernameField;
-    
+    @FXML
+    private TextField PasswordField;
 
+    @FXML
+    private Button button;
+    
+    @FXML
+    private void handleLogin() {
+        System.out.println("Hello, World!");;
+    }
 }
